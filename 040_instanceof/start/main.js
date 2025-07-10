ba913d3827c7958703d8f8191b61c73d403e8daa
kakipi16 <kakipi533@gmail.com>
2025-07-10 18:32:06 +0900

    040最後だけ微妙

--- 040_instanceof/start/main.js
+++ 040_instanceof/start/main.js
@@ -1,7 +1,43 @@
 function F(a, b) {
     this.a = a;
     this.b = b;
+    // const result = new Object();
+    // result.a = 1;
     // return {a: 1};
 }
 
 F.prototype.c = function() {}
+
+const instance = new F(1,2);
+//true
+console.log(instance instanceof F)
+//false
+console.log(instance.__proto__ === Object.prototype)
+
+function V(a, b) {
+    this.a = a;
+    this.b = b;
+    const result = new Object();
+    result.a = 1;
+    return {a: 1};
+}
+
+F.prototype.c = function() {}
+
+const instance2 = new V(1,2);
+// false
+console.log(instance2 instanceof V)
+//true
+console.log(instance2.__proto__ === Object.prototype)
+
+
+//オブジェクト指向だとよく使うそう
+function fn(arg) {
+    if(arg instanceof Array) {
+        arg.push("value")
+    } else {
+        arg["key"] = "value";
+    }
+}
+
+fn([])
