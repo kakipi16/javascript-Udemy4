7f87223b2e9a3b04f9cf18013a1cf39c4a030819
kakipi16 <kakipi533@gmail.com>
2025-07-11 17:29:27 +0900

    060学習完了

--- 060_プロトタイプチェーン/start/main.js
+++ 060_プロトタイプチェーン/start/main.js
@@ -2,10 +2,37 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
+// 階層が浅いものから呼び出される。
 Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
+  console.log('Person: hello ' + this.name);
+}
+
+Object.prototype.hello = function() {
+  console.log('Object: hello ' + this.name);
 }
 
 const bob = new Person('Bob', 18);
 bob.hello();
+
+function Person1(name, age) {
+  this.name = name;
+  this.age = age;
+  this.hello = function() {
+    console.log('OwnProperty: hello ' + this.name);
+  }
+}
+//階層が浅いものから呼び出される。
+// Person1.prototype.hello = function() {
+//   console.log('Person: hello ' + this.name);
+// }
+
+Object.prototype.hello = function() {
+  console.log('Object: hello ' + this.name);
+}
+
+const bob1 = new Person1('Bob', 18);
+bob1.hello();
+
+const result = bob1.hasOwnProperty('name')
+//true
+console.log(result);
