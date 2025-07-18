9c57859b6192698b371530b58efc2f5762a804c7
kakipi16 <kakipi533@gmail.com>
2025-07-18 18:08:56 +0900

    090学習完了

--- 090_クラス/start/main.js
+++ 090_クラス/start/main.js
@@ -1,8 +1,23 @@
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+//ES6だとこの表記で書くことが多い。
+//クラスで表記するとこうなる。
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  hello() {
+    console.log('hello ' + this.name);
+  }
 }
 
-Person.prototype.hello = function() {
-  console.log('hello ' + this.name);
-}
+const bob = new Person("Bob", 23);
+console.log(bob);
+
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+
+// Person.prototype.hello = function() {
+//   console.log('hello ' + this.name);
+// }
